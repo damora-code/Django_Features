f57dab6dbfeb5161793a804b345d548b018d4446
damora-code <55546610+damora-code@users.noreply.github.com>
2020-10-21 18:19:08 -0400

    changed 'Message' to 'Content'

--- contact/forms.py
+++ contact/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
-    Message = forms.CharField(
+    content = forms.CharField(
         required=True,
         widget=forms.Textarea
     )
