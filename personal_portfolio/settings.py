8e7c4edcd6169fbfb4793bac3f3c3e288be2f863
damora-code <55546610+damora-code@users.noreply.github.com>
2020-10-27 22:41:39 -0400

    added working .env files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,8 +129,8 @@ STATICFILES_DIRS = ( os.path.join('static'), )
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.yahoo.com'
-EMAIL_HOST_USER = 'daniel.morales117@yahoo.com' #my email
-EMAIL_HOST_PASSWORD = 'ryvzpskwshvskprx'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
